661c5bb8ec742e44df03485277587dc82f74a191
Mhd-Muzammil <zamil627810@gmail.com>
2026-04-06 21:53:19 +0530

    fix the not found '/' log err in production

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+
+def health_check(request):
+    return JsonResponse({'status': 'ok'})
+
 
 urlpatterns = [
+    path('', health_check, name='health-check'),
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('api/', include('api.urls')),
 ]
 
 if settings.DEBUG:
